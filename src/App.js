fa1dd8fc744cd022089604babfd4afebbfab3f6c
surelay <hooddevelopers@gmail.com>
2020-08-24 20:36:51 +0100

    added myheader component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Myheader from './Myheader';
+import { Button, Table} from 'react-bootstrap';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Myheader/>
+      <div className="container">
+        <div className="row mt-5">
+            <div className="col-lg-4 mb-4 grid-margin">
+              <div className="card h-100">
+                  <h4 className="card-header">Card Title</h4>
+                  <div className="card-body">
+                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+                  </div>
+                  <div className="card-footer">
+                    <Button variant="btn btn-primary">Learn More</Button>
+                  </div>
+              </div>
+            </div>
+            <div className="col-lg-4 mb-4 grid-margin">
+              <div className="card h-100">
+                  <h4 className="card-header">Card Title</h4>
+                  <div className="card-body">
+                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ipsam eos, nam perspiciatis natus commodi similique totam consectetur praesentium molestiae atque exercitationem ut consequuntur, sed eveniet, magni nostrum sint fuga.</p>
+                  </div>
+                  <div className="card-footer">
+                    <Button variant="btn btn-primary">Learn More</Button>
+                  </div>
+              </div>
+            </div>
+            <div className="col-lg-4 mb-4 grid-margin">
+              <div className="card h-100">
+                  <h4 className="card-header">Card Title</h4>
+                  <div className="card-body">
+                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+                  </div>
+                  <div className="card-footer">
+                    <Button variant="btn btn-primary">Learn More</Button>
+                  </div>
+              </div>
+            </div>
+        </div>
+        <div className="row mb-4">
+          <div className="col-sm-12 grid-margin">
+            <div className="card h-100">
+              <h4 className="card-header">Table</h4>
+              <div className="card-body">
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Username</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Mark</td>
+                      <td>Otto</td>
+                      <td>@mdo</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>Jacob</td>
+                      <td>Thornton</td>
+                      <td>@fat</td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td colSpan="2">Larry the Bird</td>
+                      <td>@twitter</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
